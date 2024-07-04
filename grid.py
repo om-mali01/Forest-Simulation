f483870e388f8f31dafb74e79917f35acc1f4e85
om-mali01 <ommali9200@gmail.com>
2024-07-04 21:06:08 +0530

    add grid and tile class

---
+++ grid.py
@@ -0,0 +1,65 @@
+from tile import Tile
+import pygame, sys
+
+class Grid:
+    def __init__(self, num_colums, num_rows, tile_width, tile_height, tile_color, screen) -> None:
+        self.num_colums = num_colums
+        self.num_rows = num_rows
+        self.tile_width = tile_width
+        self.tile_height = tile_height
+        self.tile_color = tile_color
+        self.screen = screen
+        self.tiles = []  #list to hold the tile object for grid
+
+        for i in range(num_colums):
+            row = []
+            for j in range(num_rows):
+                x = i * self.tile_width
+                y = j * self.tile_height
+                tile = Tile(x, y, self.tile_width, self.tile_height, self.tile_color)
+                row.append(tile)
+            self.tiles.append(row)
+
+    def draw(self):
+        """draw all tiles in grid"""
+        for row in self.tiles:
+            for tile in row:
+                tile.draw_tile(self.screen)
+
+    def run_loop(self):
+        """Main loop for running the grid display"""
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            self.screen.fill((255, 255, 255))
+            self.draw() #draw the grid
+            pygame.display.flip()
+
+        pygame.quit()
+        sys.exit()
+
+def main():
+    
+    pygame.init()
+
+    screen_width = 600
+    screen_height = 600
+    tile_color = (0, 0, 0) #black
+
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    
+    num_rows = 5
+    num_colums = 5
+    tile_width = screen_width // num_rows
+    tile_height = screen_height // num_colums
+
+    grid = Grid(num_colums, num_rows, tile_width, tile_height, tile_color, screen)
+
+    grid.run_loop()
+
+main()
+
+
---
+++ tile.py
@@ -0,0 +1,15 @@
+import pygame, sys
+
+class Tile:
+    def __init__(self, x, y, width, height, color) -> None:
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = pygame.Rect(x, y, width, height) #creats the rectangle/Tile
+
+    def draw_tile(self, screen):
+        # draws the tile on scrreen
+        self.screen = screen
+        pygame.draw.rect(self.screen, self.color, self.rect, 1)
